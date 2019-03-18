4937b0ee577d32e381c8c91b73af2a8fce4b4145
liuyk <1823105945@qq.com>
2019-03-18 14:06:37 +0800

    --

---
+++ src/main/java/com/example/SpringBoot/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.SpringBoot.repository;
+
+import com.example.SpringBoot.DaoUser.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+//    保存用户对象
+//    传入用户对象
+//    返回 如果保存成功返回true否则返回false
+    public boolean save(User user){
+
+    }
+
+}
